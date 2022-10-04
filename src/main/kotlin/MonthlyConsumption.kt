1fc4830e5169e05e05aaa384a0509a690ff25bc3
fatweedington <aron.binggeli@gmail.com>
2022-10-04 13:46:43 +0200

    daegg

--- src/main/kotlin/MonthlyConsumption.kt
+++ src/main/kotlin/MonthlyConsumption.kt
@@ -1,2 +1,27 @@
-class MonthlyConsumption {
+import java.time.LocalDate
+import java.time.Month
+import kotlin.math.pow
+
+class MonthlyConsumption(val beginDate: LocalDate,
+                         val endDate:LocalDate,
+                         val beginIndx:Int,
+                         val endIndx:Int,
+                         val kpcs:Double) {
+
+
+    fun getConsumptionM3():Int{
+        return endIndx - beginIndx
+    }
+
+    fun getConsumptionKWh():Double{
+        return (getConsumptionM3()*kpcs).pow(1)
+    }
+
+    fun getYear():Int{
+        return endDate.year
+    }
+
+    fun getMonth():Month{
+        return endDate.month
+    }
 }
